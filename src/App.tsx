5a1c95ec9543310a561de627b06f4fe4ceefb95e
chisambwe.k <chisambwe.k@carbonyte.io>
2023-05-23 21:46:47 +0100

    Added handling errors with axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,33 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 
-const URL = "https://jsonplaceholder.typicode.com/users";
+const URL = "https://jsonplaceholder.typicode.com/cusers";
 
 function App() {
   const [users, setUsers] = useState<User[]>([]);
+  const [error, setError] =useState('')
 
   interface User {
     id: number;
     name: string;
   }
   useEffect(() => {
-    axios.get<User[]>(URL).then((response) => setUsers(response.data));
+    axios
+      .get<User[]>(URL)
+      .then((response) => setUsers(response.data))
+      .catch((err) => setError(err.message));
   }, []);
 
   return (
+    <>
+    {error && <p className="text-danger">{error}</p>}
     <ul>
-      {users.map(({ id, name, }) => (
+      {users.map(({ id, name }) => (
         <li key={id}>{name}</li>
       ))}
     </ul>
+
+    </>
   );
 }
 export default App;
