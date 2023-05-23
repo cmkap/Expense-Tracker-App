643fb266714f9a60df9970ddc091a32c5141907a
chisambwe.k <chisambwe.k@carbonyte.io>
2023-05-23 21:03:30 +0100

    Effect clean up

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,14 @@
-import { useEffect,useState } from "react";
-import ProductList from "./components/ProductList";
+import { useEffect } from "react";
+
+const connect = () => console.log('Connecting');
+const disconnect = () => console.log('Disconnecting')
+
 
 function App() {
-const [category, setCategory]= useState('')
-  // afterRender
+  useEffect(()=> {
+   connect();
 
-
-
-  return (
-    <div>
-      <select name="" id="" className="form-select" onChange={(event) => setCategory(event.target.value)}>
-        <option value=""></option>
-        <option value="Clothing">Clothing</option>
-        <option value="Household">Household</option>
-      </select>
-      <ProductList category={category}/>
-    </div>
-  )
+   return () => disconnect();
+  })
 }
-
 export default App;
