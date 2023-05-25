47667ab77712befe940dd7aae8738c6178a43fed
chisambwe.k <chisambwe.k@carbonyte.io>
2023-05-25 19:36:37 +0100

    Updated App.js, Added delete request functionality using an optimistic update approach

--- src/App.tsx
+++ src/App.tsx
@@ -32,13 +32,29 @@ function App() {
     return () => controller.abort();
   }, []);
 
+  const deleteUser = (user:User) => {
+    const originalUsers = [...users]
+    setUsers(users.filter(u=> u.id !== user.id))
+    axios.delete(URL + `/${user.id}`).catch(error => {
+      setError(error.message)
+      setUsers(originalUsers)
+    })
+    
+
+  }
+
   return (
     <>
       {error && <p className="text-danger">{error}</p>}
       {isLoading && <div className="spinner-border"></div>}
-      <ul>
-        {users.map(({ id, name }) => (
-          <li key={id}>{name}</li>
+      <ul className="list-group ">
+        {users.map((user) => (
+          <li
+            key={user.id}
+            className="list-group-item d-flex justify-content-between"
+          >
+            {user.name} <button className="btn btn-outline-danger" onClick={() => deleteUser(user) }>Delete</button>
+          </li>
         ))}
       </ul>
     </>
