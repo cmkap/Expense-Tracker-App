fa837e224aa8594b6a3564a60effa7cd147311af
chisambwe.k <chisambwe.k@carbonyte.io>
2023-05-29 17:38:00 +0100

    Added hood dir, Added custom data fetching useUsers hook

--- src/App.tsx
+++ src/App.tsx
@@ -2,31 +2,10 @@ import { useEffect, useState } from "react";
 
 import { CanceledError } from "./services/api-client";
 import userService, { User } from "./services/user-service";
-
+import useUsers from "./hooks/useUsers";
 
 function App() {
-  const [users, setUsers] = useState<User[]>([]);
-  const [error, setError] = useState("");
-  const [isLoading, setLoading] = useState(false);
-
-  useEffect(() => {
-    setLoading(true);
-
-    const { request, cancel } = userService.getAll<User>();
-    request
-      .then((response) => {
-        setUsers(response.data);
-        setLoading(false);
-      })
-      .catch((err) => {
-        if (err instanceof CanceledError) return;
-        setError(err.message);
-        setLoading(false);
-      });
-    // .finally(() => setLoading(false)) - this is the best solution but does not work in strict mode
-
-    return () => cancel();
-  }, []);
+  const {users, error, isLoading, setUsers, setError } = useUsers()
 
   const deleteUser = (user: User) => {
     const originalUsers = [...users];
